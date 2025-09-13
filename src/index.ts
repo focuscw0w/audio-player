8cbdc6e1c37793d707aa08f52bda5d2ad0a25100
filipj169 <jamrichfilip16@gmail.com>
2025-09-13 11:23:55 +0200

    added passport and session for auth

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import path from "path";
+import session from "express-session";
+import passport from "passport";
+import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 
 import homeRouter from "./routes/home.js";
 import loginRouter from "./routes/login.js";
+
+// Config for environment variables
+dotenv.config({ path: ".env.local" });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,11 +18,24 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Public folder
+// Public folder & Views
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+// Session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, maxAge: 60000 * 60 },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 app.use("/", homeRouter);
